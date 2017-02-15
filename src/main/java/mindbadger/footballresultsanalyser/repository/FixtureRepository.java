3fecd005703c0164867771c5b4b8e5e3d76fc06f
Mark Fowler <themindbadger@gmail.com>
2017-02-15 09:30:11 +0000

    Added custom search methods to the generic Repository

--- src/main/java/mindbadger/footballresultsanalyser/repository/FixtureRepository.java
+++ src/main/java/mindbadger/footballresultsanalyser/repository/FixtureRepository.java
@@ -1,6 +1,14 @@
 package mindbadger.footballresultsanalyser.repository;
 
+import java.util.List;
+
 import mindbadger.footballresultsanalyser.domain.Fixture;
+import mindbadger.footballresultsanalyser.domain.SeasonDivision;
+import mindbadger.footballresultsanalyser.domain.Team;
 
 public interface FixtureRepository extends Repository<Fixture, String>{
+	List<Fixture> getFixturesWithNoFixtureDate();
+	List<Fixture> getFixturesForTeamInDivisionInSeason(SeasonDivision seasonDivision, Team team);
+	List<Fixture> getFixturesForDivisionInSeason(SeasonDivision seasonDivision);
+	List<Fixture> getUnplayedFixturesBeforeToday();
 }
